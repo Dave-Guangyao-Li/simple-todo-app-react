6c4d9d07d052c144a85eb89d387ab7341574a7ce
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-04-05 22:54:31 -0700

    finish two todo list boxes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BsSquare, BsFillTrashFill, BsCheckSquareFill } from "react-icons/bs";
 import { useState } from "react";
 
 function App() {
-  const [todoList, setTodoList] = useState([{ id: 1, text: "to do Item1", checked: false }, { id: 2, text: "to do Item2", checked: false }, { id: 3, text: "to do Item3", checked: true }, { id: 4, text: "to do Item4", checked: true }]); // this is the array of objects, each object is a todo item, with id, text, and checked status
+  const [todayTodoList, setTodayTodoList] = useState([{ id: 1, text: "to do Item1", checked: false }, { id: 2, text: "to do Item2", checked: false }, { id: 3, text: "to do Item3", checked: true }, { id: 4, text: "to do Item4", checked: true }]); // this is the array of objects, each object is a todo item, with id, text, and checked status
+  const [tomorrowTodoList, setTomorrowTodoList] = useState([{ id: 1, text: "to do Item5", checked: false }, { id: 2, text: "to do Item6", checked: false }, { id: 3, text: "to do Item7", checked: true }, { id: 4, text: "to do Item8", checked: true }]); // this is the array of objects, each object is a todo item, with id, text, and checked status
 
   return (
     <>
@@ -12,14 +13,14 @@ function App() {
         <div className="lists-frame">
           <div className="todo-box-1">
             <div className="title-frame">
-              <div className="title-text">Things to do</div>
+              <div className="title-text">Things to do Today</div>
             </div>
             {/* map todo list */}
-            {todoList.map((todo) => {
+            {todayTodoList.map((todo) => {
               return (
                 <div className="checkbox-item">
                   <div className="checkbox-icon" onClick={() => {
-                    setTodoList(todoList.map((item) => {
+                    setTodayTodoList(todayTodoList.map((item) => {
                       if (item.id === todo.id) {
                         return { ...item, checked: !item.checked }
                       }
@@ -38,12 +39,27 @@ function App() {
           </div>
           <div className="todo-box-2">
             <div className="title-frame">
-              <div className="title-text"></div>
+              <div className="title-text">Things to do Tomorrow</div>
             </div>
-            <div className="checkbox-item">
-              <div className="checkbox-icon"></div>
-              <div className="checkbox-text"></div>
-            </div>
+            {tomorrowTodoList.map((todo) => {
+              return (
+                <div className="checkbox-item">
+                  <div className="checkbox-icon" onClick={() => {
+                    setTomorrowTodoList(tomorrowTodoList.map((item) => {
+                      if (item.id === todo.id) {
+                        return { ...item, checked: !item.checked }
+                      }
+                      return item;
+                    }))
+                  }}>
+                    {todo.checked ? <BsCheckSquareFill /> : <BsSquare />}
+
+                  </div>
+                  {/* if checked, add line through in css on text */}
+                  <div className="checkbox-text" style={{ textDecoration: todo.checked ? "line-through" : "none" }}>{todo.text}</div>
+                </div>
+              ) // end of return
+            })} {/* end of map */}
           </div>
         </div>
       </div>
